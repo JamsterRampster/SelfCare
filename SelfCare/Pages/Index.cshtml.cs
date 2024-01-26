ea0c8851f777120bca27fa56f27f2a78642f8604
JamsterRampster <jamsterv2@gmail.com>
2024-01-26 18:40:41 +0000

    Initial Commit

--- SelfCare/Pages/Index.cshtml.cs
+++ SelfCare/Pages/Index.cshtml.cs
@@ -1,20 +1,45 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using SelfCare.Models;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace SelfCare.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly SelfCareContext _selfcareContext;
+        public List<Note> Notes { get; set; }
 
-        public IndexModel(ILogger<IndexModel> logger)
+
+        public IndexModel(ILogger<IndexModel> logger, SelfCareContext selfcareContext)
         {
             _logger = logger;
+            _selfcareContext = selfcareContext;
+
+        }
+
+
+        public IActionResult OnGetPartial()
+        {
+
+            List<Note> Notes = _selfcareContext.Notes.ToList();
+            return new PartialViewResult
+            {
+                ViewName = "_NoteTablePartial",
+                ViewData = new ViewDataDictionary(Notes)
+                //ViewData = new ViewDataDictionary<List<Note>>(ViewData, Notes)
+            };
         }
 
         public void OnGet()
         {
-
+            var zac = "fred";
         }
     }
 }
--- SelfCare/Program.cs
+++ SelfCare/Program.cs
@@ -1,6 +1,16 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using SelfCare.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<SelfCareContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
@@ -9,13 +19,13 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
+// Add routing middleware
 app.UseRouting();
 
 app.UseAuthorization();
